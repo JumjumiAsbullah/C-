b2fe5a7a0c20ea644c5bb1233abfb8b54f15ed01
JumjumiAsbullah <55911498+JumjumiAsbullah@users.noreply.github.com>
2019-10-19 16:20:23 +0700

    Add files via upload
    My Program, with C++

---
+++ NIM.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	system ("color a");
+	system("title NIM SISTEM INFORMASI 2 STAMBUK 2019 Create @Ronny Ramadhan Hasibuan");
+	int NIM = 702193174;
+	
+	
+	cout<<"    ======================================================================================================================================="<<endl;
+	cout<<"                                             NIM SISTEM INFORMASI 2 STAMBUK 2019 (CREAT : @Ronny Ramadhan Hasibuan)                                               "<<endl;
+	cout<<"    ======================================================================================================================================="<<endl;
+	cout<< "   702191107 dedyka s";   cout<<"                        702192019 nadya fatyanal";      cout<<"           702193157 amrullah fadil";     cout<<"            702193167 zulkarnain fahmi"<<endl;
+    cout<< "   702191088 dewi kartika";   cout<<"                    702192020 wahyu hari saputra";      cout<<"       702193158 nur cahaya harahap";     cout<<"        702193168 nurul arfisyah daulay"<<endl;
+    cout<< "   702191112 diana asmarani siregar";   cout<<"          702192021 umi kalsum siregar";      cout<<"       702193159 anisha fhuza hasibuan";     cout<<"     702193169 qori al-fahmi"<<endl;
+    cout<< "   702191104 dini syafriani";   cout<<"                  702192022 mhd. al-ghazaly sinaga";      cout<<"   702193160 meilisa ninta sari";     cout<<"        702193170 jumjumi asbullah"<<endl;
+    cout<< "   702191129 dwika sherlyanda";   cout<<"                702192023 rizky riphaldi";      cout<<"           702193161 erni khoiriah";     cout<<"             702193171 zepi yarti simpa"<<endl;
+    cout<< "   702191100 dzira Faza zarasky batu bara";   cout<<"    702192024 salsabila tasmara";      cout<<"        702193162 siska maya sari rambe";     cout<<"     702193172 mira pebriani berutu"<<endl;
+    cout<< "   702192015 anisa yulia";   cout<<"                     702192025 desy purwani";      cout<<"             702193163 muhammad farhan ";    cout<< "          702193173 ronny ramadhan hasibuan"<<endl;
+    cout<< "   702192016 m. idhan khalid";   cout<<"                 702192026 wafiq hafiz wardana";      cout<<"      702193164 devi apriani";     cout<<"              702193174 agung aulia tama"<<endl;
+    cout<< "   702192017 fachri fadillah nur ali";   cout<<"         702192027 isma nurlita dalimunthe";      cout<<"  702193165 nasywa fathya rosya"<<endl;
+    cout<< "   702192018 bagus setiawan";   cout<<"                  702192028 arya pratama";      cout<<"             702193166 retno tri amanda"<<endl;
+	cout<<("\n");    
+    cout<<"Masukkan NIM Anda??? ";
+    cin>>NIM;
+    cout<<("\n");
+    
+    if (NIM>702193174)
+    {
+    	cout<<"Mohon Maaf, Anda bukan Mahasiswa UINSU Juara"<<endl;
+	}
+    else if (NIM<702193174)
+    {
+    	cout<<"NIM Anda Adalah "<<NIM<< " ,Anda Mahasiswa UINSU Juara, Terima Kasih :) "<<endl;
+	}
+	cout<<("\n");
+    
+	switch (NIM){
+		case 702191107: cout<<"dedyka s"; 
+			break;
+		case 702191088: cout<<"dewi kartika";
+			break;
+		case 702191112: cout<<"diana asmarani siregar";
+			break;
+		case 702191104: cout<<"dini syafriani";
+			break;
+		case 702191129: cout<<"dwika sherlyanda";
+			break;
+		case 702191100: cout<<"dzira Faza zarasky batu bara";
+			break;
+		case 702192015: cout<<"anisa yulia";
+			break;
+		case 702192016: cout<<"m. idhan khalid";
+			break;
+		case 702192017: cout<<"fachri fadillah nur ali";
+			break;
+		case 702192018: cout<<"bagus setiawan";
+			break;
+		case 702192019: cout<<"nadya fatyanal";
+			break;
+		case 702192020: cout<<"wahyu hari saputra";
+			break;
+		case 702192021: cout<<"umi kalsum siregar";
+			break;
+		case 702192022: cout<<"mhd. al-ghazaly sinaga";
+			break;
+		case 702192023: cout<<"rizky riphaldi";
+			break;
+		case 702192024: cout<<"salsabila tasmara";
+			break;
+		case 702192025: cout<<"desy purwani";
+			break;
+		case 702192026: cout<<"wafiq hafiz wardana";	
+			break;
+		case 702192027: cout<<"isma nurlita dalimunthe";
+			break;
+		case 702192028: cout<<"arya pratama";
+			break;
+		case 702193157: cout<<"amrullah fadil";
+			break;
+		case 702193158: cout<<"nur cahaya harahap";
+			break;
+		case 702193159: cout<<"anisha fhuza hasibuan";
+			break;
+		case 702193160: cout<<"meilisa ninta sari";
+			break;
+		case 702193161: cout<<"erni khoiriah";
+			break;
+		case 702193162: cout<<"muhammad fahan azmi darus";
+			break;
+		case 702193163: cout<<"devi apriani";
+			break;
+		case 702193164: cout<<"nasywa fathya rosya";
+			break;
+		case 702193165: cout<<"retno tri amanda";
+			break;
+		case 702193166: cout<<"zulkarnain fahmi";
+			break;
+		case 702193167: cout<<"nurul arfisyah daulay";
+			break;
+		case 702193168: cout<<"qori al-fahmi";
+			break;
+		case 702193170: cout<<"jumjumi asbullah";
+			break;
+		case 702193171: cout<<"zepi yarti simpa";
+			break;
+		case 702193172: cout<<"mira pebriani berutu";
+			break;
+		case 702193173: cout<<"ronny ramadhan hasibuan";
+			break;
+		case 702193174: cout<<"agung aulia tama";
+			break;	
+	getch ();
+	
+	}                         
+}
